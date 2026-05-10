097f51a60db2916a4692172148209326f5d8fbcb
Santiago Gonzalez <santiago.gonzalez.roisler@gmail.com>
2026-05-10 20:18:33 -0300

    Sistema de roles y permisos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Perfil
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+@login_required
+def panel_admin(request):
+
+    perfil = Perfil.objects.get(usuario=request.user)
+
+    if perfil.rol != 'admin':
+        return render(request, 'sin_permiso.html')
+
+    return render(request, 'panel_admin.html')
+
+
+@login_required
+def panel_consorcista(request):
+
+    perfil = Perfil.objects.get(usuario=request.user)
+
+    if perfil.rol != 'consorcista':
+        return render(request, 'sin_permiso.html')
+
+    return render(request, 'panel_consorcista.html')
