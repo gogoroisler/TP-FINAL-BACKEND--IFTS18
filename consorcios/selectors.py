6a11bc81c5c644068d528f398aefdb4cfb94d9e3
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2026-06-17 14:38:27 -0300

    feat: titularidad activa integrada en logica de mis_expensas

--- consorcios/selectors.py
+++ consorcios/selectors.py
@@ -1,12 +1,31 @@
 from .models import Departamento, Titularidad
+from django.utils import timezone
 
 
 def get_departamento_por_usuario(usuario):
     return Departamento.objects.filter(usuario=usuario).first()
 
 
-def get_titularidad_activa(departamento):
+def get_titularidad_activa(usuario):
+    hoy = timezone.now().date()
+    return Titularidad.objects.filter(
+        usuario=usuario,
+        fecha_desde__lte=hoy,
+        fecha_hasta__isnull=True
+    ).first()
+
+
+def get_departamento_por_titularidad(usuario):
+    titularidad = get_titularidad_activa(usuario)
+    if titularidad:
+        return titularidad.departamento
+    return None
+
+
+def get_titular_en_periodo(departamento, fecha):
     return Titularidad.objects.filter(
         departamento=departamento,
+        fecha_desde__lte=fecha,
+    ).filter(
         fecha_hasta__isnull=True
-    )
+    ).first()
--- consorcios/views.py
+++ consorcios/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 
-from consorcios.selectors import get_departamento_por_usuario
+from consorcios.selectors import get_departamento_por_titularidad, get_departamento_por_usuario
 from expensas.selectors import get_expensas_por_departamento
 from expensas.models import Expensa
 
@@ -14,5 +15,11 @@ class MisExpensasView(ListView):
     context_object_name = 'expensas'
 
     def get_queryset(self):
-        departamento = get_departamento_por_usuario(self.request.user)
-        return get_expensas_por_departamento(departamento)
+        # Primero busca por titularidad activa
+        departamento = get_departamento_por_titularidad(self.request.user)
+        # Si no tiene titularidad, cae al campo usuario directo
+        if not departamento:
+            departamento = get_departamento_por_usuario(self.request.user)
+        if departamento:
+            return get_expensas_por_departamento(departamento)
+        return Expensa.objects.none()
